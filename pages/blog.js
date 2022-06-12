d4c304d52615a697bdf080dfd60c0921792033c8
yogitheboss <yograjrjpt123@gmail.com>
2022-06-12 22:23:47 +0530

    commit on tutorial-no-12

---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>this is a blog page</div>
+  )
+}
+
+export default blog
---
+++ pages/components/dummy.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Dummy = () => {
+  return (
+    <>
+      <style jsx>
+        {`
+          .dummy {
+            background: yellow ;
+          }
+        `}
+      </style>
+      <div className="dummy">Dummy</div>
+    </>
+  );
+};
+
+export default Dummy;
